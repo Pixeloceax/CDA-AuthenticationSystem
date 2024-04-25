d5bdfeacfd4fbaec05495fd63012b6a6502795b0
Axel <axel.valentinbvs@yahoo.com>
2024-04-25 23:44:21 +0200

    feat: updateUserInformation

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import isset from "../utils/isset";
 
 const userModel = require("../models/userModel");
 
@@ -12,6 +13,29 @@ export const getUserInformation = async (req: Request, res: Response) => {
 
     res.status(200).json(user);
   } catch (error) {
-    throw new Error(`USER ERROR : ${error}`);
+    throw new Error(`GET USER ERROR : ${error}`);
+  }
+};
+
+export const updateUserInformation = async (req: Request, res: Response) => {
+  try {
+    const user = await userModel.findOne({ _id: req.user });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    if (isset(req.body)) {
+      Object.keys(req.body).forEach((key) => {
+        if (isset(req.body[key]) && isset(user[key])) {
+          user[key] = req.body[key];
+        }
+      });
+    }
+
+    await user.save();
+
+    res.status(200).json("User updated");
+  } catch (error) {
+    throw new Error(`UPDATE USER ERROR : ${error}`);
   }
 };
