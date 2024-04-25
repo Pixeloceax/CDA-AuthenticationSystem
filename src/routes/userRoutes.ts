975e871d4a09af5821bdfb3e8f832fda70c718d4
Axel <axel.valentinbvs@yahoo.com>
2024-04-25 23:43:58 +0200

    feat: add updateUserInformation endpoint /update

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,11 @@
 import express from "express";
-import { getUserInformation } from "../controllers/userController";
+import {
+  getUserInformation,
+  updateUserInformation,
+} from "../controllers/userController";
 const router = express.Router();
 
 router.get("/profile", getUserInformation);
+router.put("/update", updateUserInformation);
 
 export default router;
