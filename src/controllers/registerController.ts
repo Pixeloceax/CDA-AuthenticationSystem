30280ea28a3383ef4960a954bc2f05fb113a07cb
Axel <axel.valentinbvs@yahoo.com>
2024-04-26 11:13:22 +0200

    feat: implemente validation schema for email and password

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -1,13 +1,35 @@
 import { Request, Response } from "express";
 import bcryptjs from "bcryptjs";
+
+import { validateEmail } from "../utils/emailValidationSchema";
+import { validatePassword } from "../utils/passwordValidationSchema";
+
 const userModel = require("../models/userModel");
 
 export const registerController = async (req: Request, res: Response) => {
   try {
-    const { fullName, email, password, phoneNumber } = req.body;
+    const { fullName, email, password, phoneNumber } = req.body as {
+      fullName: string;
+      email: string;
+      password: string;
+      phoneNumber: string;
+    };
+
+    const isEmailValid = validateEmail(email);
+    if (!isEmailValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid email format" });
+    }
+
+    const isPasswordValid = validatePassword(password);
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid password format" });
+    }
 
     const existingUser = await userModel.findOne({ email });
-
     if (existingUser) {
       return res
         .status(400)
