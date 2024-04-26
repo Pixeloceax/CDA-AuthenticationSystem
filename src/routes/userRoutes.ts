e52ce5968da54789ed02f7371152bdbf535fe206
Axel <axel.valentinbvs@yahoo.com>
2024-04-26 09:53:32 +0200

    feat: add /user/delete endpoint

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,10 +2,12 @@ import express from "express";
 import {
   getUserInformation,
   updateUserInformation,
+  deleteUser,
 } from "../controllers/userController";
 const router = express.Router();
 
 router.get("/profile", getUserInformation);
 router.put("/update", updateUserInformation);
+router.delete("/delete", deleteUser);
 
 export default router;
